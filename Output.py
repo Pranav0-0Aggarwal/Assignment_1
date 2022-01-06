c13758e256bf772c2c4634f59c598b0923ada524
Pranav0-0Aggarwal <96537779+Pranav0-0Aggarwal@users.noreply.github.com>
2022-01-06 16:18:09 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,47 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: /Users/pranavaggarwal/Documents/Python Clg/1.py ===========
+Question 1
+Plz Give an Input for first number- 25
+Plz Give an Input for second number- 27
+Plz Give an Input for third number- 39
+30.333333333333332
+
+=========== RESTART: /Users/pranavaggarwal/Documents/Python Clg/2.py ===========
+Question 2
+Enter your gross Income sir-$20000
+Enter total number of Dependants-1
+Your tax = $1400.0
+
+=========== RESTART: /Users/pranavaggarwal/Documents/Python Clg/3.py ===========
+Question 3
+SID-21105095
+Name-Pranav Aggarwal
+Gender(F,M orU)-M
+Course name-ECE
+CGPA-10
+[21105095, 'Pranav Aggarwal', 'M', 'ECE', 10.0]
+
+=========== RESTART: /Users/pranavaggarwal/Documents/Python Clg/4.py ===========
+Question 4
+Plz give marks as Input
+
+13
+23
+15
+17
+19
+*************************
+13
+15
+17
+19
+23
+
+=========== RESTART: /Users/pranavaggarwal/Documents/Python Clg/5.py ===========
+Question 5
+Part a
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b
+['Red', 'Green', 'White', 'Purple', 'Yellow']
