c4b37a57317a13129de3acc1fcbe59b875a9b544
Pranav0-0Aggarwal <96537779+Pranav0-0Aggarwal@users.noreply.github.com>
2022-02-06 15:26:27 +0530

    Update output_asg3.py

--- Assignment 3/output_asg3.py
+++ Assignment 3/output_asg3.py
@@ -28,3 +28,57 @@ Please enter Day- 29
 Please enter Month- 02
 Please enter Year- 2020
 Next Date is: 1/2/2020
+
+===== RESTART: /Users/pranavaggarwal/Documents/Python Clg/Assignment 3/3.py ====
+Question 3
+[3, 5, 8, 12, 15]
+[(3, 9), (5, 25), (8, 64), (12, 144), (15, 225)]
+
+===== RESTART: /Users/pranavaggarwal/Documents/Python Clg/Assignment 3/4.py ====
+Question 4
+Enter Grade Points- 8
+Your Grade is B+ and Very Good performance 
+
+===== RESTART: /Users/pranavaggarwal/Documents/Python Clg/Assignment 3/5.py ====
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A 
+ 
+===== RESTART: /Users/pranavaggarwal/Documents/Python Clg/Assignment 3/6.py ====
+Question 6
+Please Enter SID of Student- 21105095
+Please enter the name of the Student- Pranav Aggarwal
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105079
+Please enter the name of the Student- Vanshul Goyal
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105026
+Please enter the name of the Student- Muskaan Aneja
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105030
+Please enter the name of the Student- Gunjan Rawat
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105078
+Please enter the name of the Student- Vedansh Singh
+If you want to enter more details press Y/N- N
+Part a
+21105095 is Pranav Aggarwal
+21105079 is Vanshul Goyal
+21105026 is Muskaan Aneja
+21105030 is Gunjan Rawat
+21105078 is Vedansh Singh
+
+Part b
+value sorted dictionary is ['Gunjan Rawat', 'Muskaan Aneja', 'Pranav Aggarwal', 'Vanshul Goyal', 'Vedansh Singh']
+
+Part c
+Keys sorted dictionary is [21105026, 21105030, 21105078, 21105079, 21105095]
+
+Part D
+Please enter the student's SID whose detail you need- 21105095
+Name of the required student is Pranav Aggarwal
+
