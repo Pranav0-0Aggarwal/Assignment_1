75675bb64ed4982915a54eb2459af4d15dff52d2
Pranav0-0Aggarwal <96537779+Pranav0-0Aggarwal@users.noreply.github.com>
2022-02-06 15:18:08 +0530

    Create output_asg3.py

---
+++ Assignment 3/output_asg3.py
@@ -0,0 +1,30 @@
+===== RESTART: /Users/pranavaggarwal/Documents/Python Clg/Assignment 3/1.py ====
+Question 1
+Please give an input- Aggarwal
+Total occurances
+a-3
+g-2
+r-1
+w-1
+l-1
+
+===== RESTART: /Users/pranavaggarwal/Documents/Python Clg/Assignment 3/1.py ====
+Question 1
+Please give an input- Pranav Aggarwal pranav aggarwal pRANAV pranav
+Total occurances
+pranav-4
+aggarwal-2
+
+===== RESTART: /Users/pranavaggarwal/Documents/Python Clg/Assignment 3/2.py ====
+Question 2
+Please enter Day- 31
+Please enter Month- 12
+Please enter Year- 2021
+Next Date is: 1/1/2022
+
+===== RESTART: /Users/pranavaggarwal/Documents/Python Clg/Assignment 3/2.py ====
+Question 2
+Please enter Day- 29
+Please enter Month- 02
+Please enter Year- 2020
+Next Date is: 1/2/2020
