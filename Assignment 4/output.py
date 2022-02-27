0e6e7d39dbb25badaface80d7ffc13e23358500f
Pranav0-0Aggarwal <96537779+Pranav0-0Aggarwal@users.noreply.github.com>
2022-02-27 22:37:26 +0530

    Create output.py

---
+++ Assignment 4/output.py
@@ -0,0 +1,100 @@
+= RESTART: /Users/pranavaggarwal/Documents/Python college/Assignment 4/asg4.py =
+Question 1
+Please enter total number of disks- 5
+Move disk 1 from A to B
+Move disk 2 from A to B
+Move disk 1 from B to A
+Move disk 3 from A to C
+Move disk 1 from C to B
+Move disk 2 from C to B
+Move disk 1 from B to C
+Move disk 4 from A to B
+Move disk 1 from B to A
+Move disk 2 from B to A
+Move disk 1 from A to B
+Move disk 3 from B to C
+Move disk 1 from C to A
+Move disk 2 from C to A
+Move disk 1 from A to C
+Move disk 5 from A to C
+Move disk 1 from C to B
+Move disk 2 from C to B
+Move disk 1 from B to C
+Move disk 3 from C to A
+Move disk 1 from A to B
+Move disk 2 from A to B
+Move disk 1 from B to A
+Move disk 4 from C to B
+Move disk 1 from B to C
+Move disk 2 from B to C
+Move disk 1 from C to B
+Move disk 3 from B to A
+Move disk 1 from A to C
+Move disk 2 from A to C
+Move disk 1 from C to A
+Total number of steps to be followed is 31
+
+
+Question 2
+Enter the number of rows in Pascal's Triangle: 5
+
+Using recursions:
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+Using loops:
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+
+
+Question 3
+Enter the first number: 19
+Enter the first number: 37
+Questiont:0
+Remainder:19
+a)
+True
+b)
+All the values are not non zero
+c)
+After adding 4,5,6 :  [0, 19, 4, 5, 6]
+Values greater than 4 are :  [19, 5, 6]
+d)
+{0, 4, 5, 6, 19}
+e)
+Immutable set :  frozenset({0, 19, 4, 5, 6})
+f)
+-6983640545602352529
+
+
+Question 4
+Object is created
+The name of the student is Pranav Aggarwal and SID is 21105095.
+Object destroyed
+
+
+Question 5
+Part a
+The updated salary of the employee Mehak is 70000
+Part B
+Employee Viren record deleted
+Records of Viren are deleted
+
+
+Question 6
+Enter the first word: vanarp
+Enter a new meaningful word to test your friendship: pranav
+
+Does the word makes sense?(y or n)
+n
+The word doesn't have a meaning, friendship is fake!!
+
